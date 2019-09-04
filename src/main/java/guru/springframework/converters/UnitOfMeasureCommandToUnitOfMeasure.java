0d54aa9f03d4fb514677736b2e547f3d906138e8
wadziuxxx <macwadowski@tlen.pl>
2019-09-04 21:03:08 +0200

    converters with tests

---
+++ src/main/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,21 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand unitOfMeasureCommand) {
+        if (unitOfMeasureCommand == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(unitOfMeasureCommand.getId());
+        uom.setDescription(unitOfMeasureCommand.getDescription());
+        return uom;
+    }
+}
